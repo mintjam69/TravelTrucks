aee9c8ab65a0dd072f89226ed5a757a393db58e7
Taras Pererva <taraspererva@gmail.com>
2025-07-20 13:45:45 +0300

    add redux

---
+++ src/redux/filter/slice.js
@@ -0,0 +1,62 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+  location: '',
+  form: '',
+  AC: false,
+  transmission: false,
+  kitchen: false,
+  TV: false,
+  bathroom: false,
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setLocation: (state, action) => {
+      state.location = action.payload;
+    },
+    setForm: (state, action) => {
+      state.form = action.payload;
+    },
+    toggleAC: state => {
+      state.AC = !state.AC;
+    },
+    setTransmission: state => {
+      state.transmission = !state.transmission;
+    },
+    toggleKitchen: state => {
+      state.kitchen = !state.kitchen;
+    },
+    toggleTV: state => {
+      state.TV = !state.TV;
+    },
+    toggleBathroom: state => {
+      state.bathroom = !state.bathroom;
+    },
+    resetFilters: (state) => {
+      state.location = '';
+      state.form = '';
+      state.AC = false;
+      state.transmission = false;
+      state.kitchen = false;
+      state.TV = false;
+      state.bathroom = false;
+    },
+  },
+});
+
+export const {
+  setLocation,
+  setForm,
+  toggleAC,
+  setTransmission,
+  toggleKitchen,
+  toggleTV,
+  toggleBathroom,
+  resetFilters,
+} = filterSlice.actions;
+
+export default filterSlice.reducer;
